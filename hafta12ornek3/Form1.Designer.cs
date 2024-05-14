078b9c2b3b0a865cd8b83601814c5757d55bb7d9
Irem <cetinkol9226@gmail.com>
2024-05-15 00:45:17 +0300

    0

---
+++ hafta12ornek3/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace hafta12ornek3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_trh = new System.Windows.Forms.TextBox();
+            this.txt_grd = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txt_trh);
+            this.groupBox1.Location = new System.Drawing.Point(191, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(390, 166);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.txt_grd);
+            this.groupBox2.Location = new System.Drawing.Point(191, 240);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(390, 166);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // txt_trh
+            // 
+            this.txt_trh.Location = new System.Drawing.Point(161, 57);
+            this.txt_trh.Name = "txt_trh";
+            this.txt_trh.Size = new System.Drawing.Size(166, 20);
+            this.txt_trh.TabIndex = 0;
+            // 
+            // txt_grd
+            // 
+            this.txt_grd.Location = new System.Drawing.Point(161, 77);
+            this.txt_grd.Name = "txt_grd";
+            this.txt_grd.Size = new System.Drawing.Size(166, 20);
+            this.txt_grd.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(205, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Set the threshold";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(205, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Check the grade";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(28, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Threshold value :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(28, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Grade value :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_trh;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_grd;
+    }
+}
+
---
+++ hafta12ornek3/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hafta12ornek3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int thresold=0;
+            thresold = int.Parse(txt_trh.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int thresold = 0;
+            thresold = int.Parse(txt_trh.Text);
+
+            int grade=int.Parse(txt_grd.Text);
+            if (grade >= 90 && thresold<=grade)
+            {
+                MessageBox.Show("A", "Geçtin");
+            }
+            else if (grade >= 80 && thresold<=grade)
+            {
+                MessageBox.Show("B", "Geçtin");
+            }
+            else if(grade >= 70 && thresold <= grade)
+            {
+                MessageBox.Show("C", "Geçtin");
+            }
+            else if(grade >= 60 && thresold <= grade)
+            {
+                MessageBox.Show("D", "Geçtin");
+            }
+            else if(grade >=50 && thresold <= grade)
+            {
+                MessageBox.Show("E", "Geçtin");
+            }
+            else
+            {
+                MessageBox.Show("F", "Kaldın");
+            }
+        }
+    }
+}
